b3093327dc60572da8daca0de7d89f93e82ff479
MattiaR <rovitomattia91@gmail.com>
2021-06-08 14:47:19 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,48 @@
+//? STEP 1 Il computer deve generare 16 numeri casuali tra 1 e 100.
+
+//? STEP 2 I numeri non possono essere duplicati. 
+
+//? STEP 3 Il computer, in seguito, deve chiedere all’utente (84) volte di inserire un numero compreso tra 1 e 100, uno alla volta.
+
+//? STEP 4 L’utente non può inserire più volte lo stesso numero. 
+
+//? STEP 5 Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+
+//? STEP 6 La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti. 
+
+//? STEP 7 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali: con difficoltà 0 => tra 1 e 100 con difficoltà 1 => tra 1 e 80 con difficoltà 2 => tra 1 e 50.
